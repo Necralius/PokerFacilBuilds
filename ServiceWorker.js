e7d7c93855c66b159757997ed977d3fcc4a4974e
Nekralius <81307194+Necralius@users.noreply.github.com>
2024-08-09 11:19:38 -0300

    Uncompressed

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "NekraByte-PPPokerPlay-7.0";
 const contentToCache = [
-    "Build/Web PokerFacil - 7.1 - PWA.loader.js",
-    "Build/Web PokerFacil - 7.1 - PWA.framework.js.gz",
-    "Build/Web PokerFacil - 7.1 - PWA.data.gz",
-    "Build/Web PokerFacil - 7.1 - PWA.wasm.gz",
+    "Build/PokerFacil (PWA) 7.0 - Uncompressed.loader.js",
+    "Build/PokerFacil (PWA) 7.0 - Uncompressed.framework.js",
+    "Build/PokerFacil (PWA) 7.0 - Uncompressed.data",
+    "Build/PokerFacil (PWA) 7.0 - Uncompressed.wasm",
     "TemplateData/style.css"
 
 ];
